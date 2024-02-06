ba0ad299110be21a22a60fedb2a66f46b8ba4523
LeviSecula <levi.secula@students.fhu.edu>
2024-02-06 09:26:18 -0600

    Solution
    Just create a new string that's reversed through slicing, then use built in compare function.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def palindrome(word):
-    pass
+def palindrome(raw_input):
+    x = raw_input[::-1]
+    return x == raw_input
+
 
 #YOUR CODE GOES HERE
+
+raw_input = input()
+print(palindrome(raw_input))
